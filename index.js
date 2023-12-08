8a940e7dcd1078faf7216ba0add705db554d4734
AshishRawat08 <151005145+AshishRawat08@users.noreply.github.com>
2023-12-08 14:07:13 +0000

    day1

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 8081;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up and running :-",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
